eb75011d27eb0092f6292c344abb0b660b0c4a49
Kandy-Kane <frankmyster@gmail.com>
2023-09-06 17:02:00 -0400

    Added console log for dailys

--- app/daily.js
+++ app/daily.js
@@ -2,9 +2,17 @@ import React from "react"
 import "./daily.css"
 
 export default function Daily(props) {
+    function handleClick(){
+        if(props.iscompleted == false){
+            console.log('daily Completed')
+        }else if(props.iscompleted == true){
+            console.log('daily Incomplete')
+        }
+        
+    }
     if(props.iscompleted == false){
         return(
-            <div className="daily-false">
+            <div className="daily-false" onClick={handleClick}  >
         <img src="/vercel.svg" alt="" />
             <h3>{props.title}</h3>
             <p>Streak: {props.streak}</p>
@@ -15,7 +23,7 @@ export default function Daily(props) {
         )
     }else{
         return(
-            <div className="daily-true">
+            <div className="daily-true" onClick={handleClick}>
         <img src="/vercel.svg" alt="" />
             <h3>{props.title}</h3>
             <p>Streak: {props.streak}</p>
